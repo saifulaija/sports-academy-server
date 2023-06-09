3bf2d547f8af89e2dbf0c18fa020fbd452620c06
saifulaija <saifulaija2023@gmail.com>
2023-06-09 15:05:36 +0600

    role api created

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000
-
+const port = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-  origin: '*',
+  origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
-app.use(express.json())
-
-
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfbgofj.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,80 +23,94 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-   
-    const usersCollection = client.db('sportsDb').collection('users')
 
+    const usersCollection = client.db("sportsDb").collection("users");
 
     // Users related api
 
-    // get all users 
+    // get all users
 
-    app.get('/users', async(req, res)=>{
-      const result = await usersCollection.find().toArray()
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    //     save user email and role in db
+
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
+    //Make Admin
+
+    app.patch("/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // Make instructor
+
+    app.patch("/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+
+    // get role
+
+    app.get('/role/:email', async(req, res)=>{
+      const email = req.params.email 
+      const query = {email: email}
+      const result = await usersCollection.findOne(query)
+      console.log(result)
       res.send(result)
     })
 
-
-
-//     save user email and role in db
-
-app.put('/users/:email', async(req, res)=>{
-      const email = req.params.email
-      const user = req.body
-      const query = {email: email}
-      const options = {upsert: true}
-      const updateDoc={
-            $set: user,
-      }
-      const result = await usersCollection.updateOne(query, updateDoc, options)
-      res.send(result)
-})
-
-//
-
-app.patch('/admin/:id', async(req, res)=>{
-  const id = req.params.id;
-  const filter = {_id: new ObjectId(id)}
-  const updateDoc={
-    $set: {
-      role: 'admin'
-    },
-  };
-  const result = await usersCollection.updateOne(filter, updateDoc)
-  res.send(result)
-})
-
-
-
-
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-//     await client.close();
+    //     await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Sports DB is Running.....");
+});
 
-
-
-
-
-
-app.get('/', (req, res)=>{
-      res.send('Sports DB is Running.....')
-})
-
-app.listen(port, ()=>{
-      console.log(`Sports DB is running on pport: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Sports DB is running on port: ${port}`);
+});
